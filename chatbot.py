76dfcc4ef5cf18c09406e43ed0e2fbfb876c7983
Chapinguin <112920689+Chapinguin@users.noreply.github.com>
2025-05-21 12:48:22 -0600

    Chapa Commit
    Version beta, faltan pruebas

--- chatbot.py
+++ chatbot.py
@@ -4,20 +4,19 @@ import pickle
 import numpy as np
 import tensorflow as tf
 
-import nltk
 from nltk.stem import WordNetLemmatizer
-from tensorflow.keras.models import load_model # type: ignore
+import nltk
 
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json').read())
 
+intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
-model = load_model('chatbot_model.h5')
+model = tf.keras.models.load_model('chatbot_model.h5')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
 def bag_of_words(sentence):
@@ -32,24 +31,20 @@ def bag_of_words(sentence):
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
-    ERROR_THRESHOLD = 0.25
+    ERROR_THRESHOLD = 0.10  # Reducido de 0.25 a 0.10
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    ...
     
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
-        return_list.append({'intent':classes[r[0]], 'probability': str(r[1])})
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return return_list
 
-def get_response (intents_list, intents_json, user_message=None):
+def get_response(intents_list, intents_json):
     if not intents_list:
-        with open("notUnderstand.txt", "a", encoding="utf-8") as f:
-            f.write(user_message + "\n")
-        return "Disculpa, No entendí eso. Podrías repetirlo?"
-    #print('Predicted probability:', intents_list[0]['probability'])
-    if float(intents_list[0]['probability']) < 0.30:
-        with open("lowProbability.txt", "a",encoding="utf-8") as f:
-            f.write(user_message + "\n")
+        return "No entendí tu mensaje. ¿Puedes reformularlo?"
+    
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
@@ -63,6 +58,5 @@ print ("Chatbot is running...")
 while True:
     message = input("")
     ints = predict_class(message)
-    res = get_response(ints, intents, user_message=message)
+    res = get_response(ints, intents)
     print(res)
-    
