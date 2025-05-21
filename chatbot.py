e062ed59eb7f2abb8711bd473621caf554399c2b
Seeker-17 <gus.cortes.orta@gmail.com>
2025-05-21 09:05:19 -0600

    Hotel_chatbot V1.0
    - El archivo de entrenamiento quedó listo.
    
    - Se cambiaron algunos detalles en el archivo chatbot.py el modo de importar el modulo load_model de tensorflow, la extensión del archivo chatbot_model de .model a .h5
    
    -Se añadió que cuando el chatbot no entienda algo, cree un archivo de texto que guarde lo que no entendió, así como se crea un archivo para guardar los mensajes con probabilidad muy baja, para reentrenar.

--- chatbot.py
+++ chatbot.py
@@ -6,15 +6,14 @@ import tensorflow as tf
 
 import nltk
 from nltk.stem import WordNetLemmatizer
-from tensorflow import load_model
-
+from tensorflow.keras.models import load_model # type: ignore
 
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
 
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
-model = load_model('chatbot_model.model')
+model = load_model('chatbot_model.h5')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
@@ -42,7 +41,15 @@ def predict_class(sentence):
         return_list.append({'intent':classes[r[0]], 'probability': str(r[1])})
     return return_list
 
-def get_response (intents_list, intents_json):
+def get_response (intents_list, intents_json, user_message=None):
+    if not intents_list:
+        with open("notUnderstand.txt", "a", encoding="utf-8") as f:
+            f.write(user_message + "\n")
+        return "Disculpa, No entendí eso. Podrías repetirlo?"
+    #print('Predicted probability:', intents_list[0]['probability'])
+    if float(intents_list[0]['probability']) < 0.30:
+        with open("lowProbability.txt", "a",encoding="utf-8") as f:
+            f.write(user_message + "\n")
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
@@ -56,6 +63,6 @@ print ("Chatbot is running...")
 while True:
     message = input("")
     ints = predict_class(message)
-    res = get_response(ints, intents)
+    res = get_response(ints, intents, user_message=message)
     print(res)
     
